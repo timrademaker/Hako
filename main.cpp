9c77fc0c5041a92261ca054a1dfa3d7c92e0f56c
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-11-28 16:51:10 +0100

    Added stand-alone mode

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
+#include "Hako/inc/Hako.h"
+
+using hako::Hako;
 int main(int argc, char* argv[])
 {
-    return 0;
+    if (argc < 3)
+    {
+        printf("Not enough arguments provided!\n");
+        // TODO: Print help
+        return 1;
+    }
+
+    std::string archiveName = std::string(argv[1]);
+
+    std::vector<std::string> files;
+
+    for (int i = 2; i < argc; ++i)
+    {
+        files.push_back(argv[i]);
+    }
+
+    bool success = Hako::CreateArchive(files, archiveName);
+    if (success)
+    {
+        printf("Successfully created archive %s\n", archiveName.c_str());
+    }
+    else
+    {
+        printf("Failed to create archive %s\n", archiveName.c_str());
+    }
+
+    return success ? 0 : 1;
 }
