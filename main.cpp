b2923ea70f779d5939464cf442f5d81fffb42202
timrademaker <10331650+timrademaker@users.noreply.github.com>
2022-01-18 22:28:38 +0100

    Renamed Hako library to HakoLib, and renamed executable to Hako

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char* argv[])
     if (argc < 3)
     {
         printf("Not enough arguments provided!\n");
-        printf("Usage: HakoContentPacker <output_archive_name> <file_to_archive> [<files_to_archive>...]\n");
-        printf("Example: HakoContentPacker Content.bin a.txt b.txt\n");
+        printf("Usage: Hako <output_archive_name> <file_to_archive> [<files_to_archive>...]\n");
+        printf("Example: Hako Content.bin a.txt b.txt\n");
         return 1;
     }
 
